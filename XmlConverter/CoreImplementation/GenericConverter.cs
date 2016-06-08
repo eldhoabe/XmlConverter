8caddbbfb03629971cfb871e09a385be311c12b7
Eldho Abe <eldhoabe.07@gmail.com>
2016-06-08 15:13:53 +0530

    Generic Xml Converter

---
+++ XmlConverter/CoreImplementation/GenericConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using XmlConverter.Interface;
+
+namespace XmlConverter.CoreImplementation
+{
+    public class GenericConverter<T> : IConverter<T> where T : class
+    {
+        readonly string FilePath;
+
+        public GenericConverter(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+                throw new ArgumentNullException("The file path is null or empty");
+
+
+            this.FilePath = filePath.ToString();
+        }
+
+        /// <summary>
+        /// Generate an Xml file
+        /// </summary>
+        /// <typeparam name="T">the type of data</typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool GenereateXml<T>(T value)
+        {
+            try
+            {
+                XmlSerializer serialiser = new XmlSerializer(typeof(T));
+                using (TextWriter Filestream = new StreamWriter(FilePath))
+                {
+                    serialiser.Serialize(Filestream, value);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ XmlConverter/Interface/IConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlConverter.Interface
+{
+    /// <summary>
+    /// Converter Interface
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IConverter<T> where T : class
+    {
+        bool GenereateXml<T>(T value);
+    }
+}
